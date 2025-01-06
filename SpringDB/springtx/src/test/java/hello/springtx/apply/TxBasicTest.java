7b590578a877fa530f5022451183e8209a1d5e7b
hyerim98 <choihyerim252@gmail.com>
2025-01-06 10:53:31 +0900

    스프링 DB 2편 - 스프링 트랜잭션 이해

---
+++ SpringDB/springtx/src/test/java/hello/springtx/apply/TxBasicTest.java
@@ -0,0 +1,64 @@
+package hello.springtx.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@Slf4j
+@SpringBootTest
+public class TxBasicTest {
+    // 실제 basicService 객체 대신에 프록시인 basicService$$CGLIB를 스프링 빈에 등록, 그리고 프록시는 내부에 실제 basicService를 참조
+    @Autowired BasicService basicService;
+
+    // proxy가 적용되었는지 확인
+    @Test
+    void proxyCheck() {
+        log.info("aop class = {}", basicService.getClass());
+        assertThat(AopUtils.isAopProxy(basicService)).isTrue();
+    }
+
+    @Test
+    void txTest() {
+        basicService.tx();
+        basicService.nonTx();
+    }
+
+    @TestConfiguration
+    static class TxAppltBasicConfig {
+        @Bean
+        BasicService basicService() {
+            return new BasicService();
+        }
+    }
+
+    @Slf4j
+    static class BasicService {
+
+        @Transactional // 트랜잭션 AOP는 프록시를 만들어서 스프링 컨테이너에 등록함(실제 객체 대신에 트랜잭션을 처리해주는 프록시 객체가 스프링 빈에 등록 됨)
+        public void tx() {
+            log.info("call tx");
+            // 트랜잭션 적용 여부
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active = {}", txActive);
+        }
+
+        public void nonTx() {
+            log.info("call nonTx");
+            // 트랜잭션 적용 여부
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("nonTx active = {}", txActive);
+        }
+    }
+
+
+}
